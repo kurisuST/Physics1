7c0f857ecdcd0c55432befccd356e3b6b6dcee27
kurisuST <crisaronbb59@gmail.com>
2024-02-19 22:48:37 -0600

    Agregar archivos de proyecto.

---
+++ Physics1/Canyon.cs
@@ -0,0 +1,33 @@
+﻿namespace Physics1
+{
+	internal class Canyon
+	{
+		//Properties
+		protected int bWeight;
+		public int BWeight
+
+		{
+			get { return bWeight; }
+			set { bWeight = value; }
+		}
+
+		protected int acc;
+		public int Acc
+		{
+			get { return acc; }
+			set { acc = value; }
+		}
+		//Constructors
+		public Canyon(int bWeight, int acc)
+		{
+			BWeight =  bWeight;
+			Acc = acc;
+		}
+		//Methods
+
+		public int GetFinalValue()
+		{
+			return BWeight*Acc;
+		}
+    }
+}
---
+++ Physics1/Program.cs
@@ -0,0 +1,18 @@
+﻿using Physics1;
+
+namespace AppClassTest
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Write the bullet weight(kg): ");
+            int bWeight = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Write the bullet acceleration(m/s): ");
+            int acc = Convert.ToInt32(Console.ReadLine());
+            Canyon bulletForce = new Canyon (bWeight, acc);
+            int FinalForce = bulletForce.GetFinalValue();
+            Console.WriteLine(FinalForce);
+        }
+    }
+}
